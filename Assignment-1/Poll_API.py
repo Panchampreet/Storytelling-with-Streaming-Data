5a93fd70a3b9d1061c0c4fc5ab2d5d272c353231
Panchampreet <pk2506@columbia.edu>
2016-03-03 04:09:01 -0500

    Update Poll_API.py
    Code to poll the streaming Times Newswire API

--- Assignment-1/Poll_API.py
+++ Assignment-1/Poll_API.py
@@ -1,1 +1,13 @@
+import requests
+import json
+import time
 
+while True:
+    # Fetching the response from NY Times through the API
+    r = requests.get("http://api.nytimes.com/svc/news/v3/content/nyt/all/24.json?api-key=2bf845abbb668b5d43da6f6387793f27:1:74611818")
+    
+	# Polling for recently saved articles on the site and displaying on standard output
+    for m in r.json()["results"]:
+        print json.dumps(m, indent=1)
+	
+    time.sleep(5)
