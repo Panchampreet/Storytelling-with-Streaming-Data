b3d93aefa91eed15f6045a559c63fa5ad84e6591
Pancham <pk2506@columbia.edu>
2016-03-06 12:28:18 -0500

    Update poll_API.py

--- Assignment-2/poll_API.py
+++ Assignment-2/poll_API.py
@@ -4,7 +4,7 @@ import sys
 import requests
 
 while True:
-        # Fetching the response from NY Times through the API
+       # Polling TimesNewswire API for recent news in the past 24 hours
         r = requests.get("http://api.nytimes.com/svc/news/v3/content/nyt/all/24.json?api-key=2bf845abbb668b5d43da6f6387793f27:1:74611818")
     
         # Polling for recently saved articles on the site and displaying on standard output
