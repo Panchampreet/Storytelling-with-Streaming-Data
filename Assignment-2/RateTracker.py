f289f1c84919acb5356811df3c199f5d5555cf7c
Pancham <pk2506@columbia.edu>
2016-03-06 19:31:28 -0500

    Update RateTracker.py

--- Assignment-2/RateTracker.py
+++ Assignment-2/RateTracker.py
@@ -10,34 +10,35 @@ rateTracker = Flask(__name__)
 def main():
 	return render_template('index.html')
 
-  lastInstance = 0
+        lastInstance = 0
 
-  while True:
- 	line = sys.stdin.readline()
-        save = json.loads(line)
-	
+        while True:
+      	        line = sys.stdin.readline()
+                save = json.loads(line)
+	     
         if lastInstance == 0 :
-             lastInstance = present
-             continue
+                lastInstance = present
+                continue
+         
         rate = present - lastInstance
         print "\n"
 
-       # Creating alerts associated to erratic behavior of the stream rate
+        # Creating alerts associated to erratic behavior of the stream rate
 	
-       # Case 1 :- If the rate falls below 20% of the specified value
+        # Case 1 :- If the rate falls below 20% of the specified value
         if rate<1:
-              message = "This rate of influx of articles is too fast!"
-              d = {rate: message}    
+                message = "This rate of influx of articles is too fast!"
+                d = {rate: message}    
 	
         # Case 2 :- If the rate grows 200% of the specified value	
         elif rate>10:
-               message = "This rate of influx of articles is very slow."
-               d = {rate:message}
+                message = "This rate of influx of articles is very slow."
+                d = {rate:message}
 			
         else:
-              message = "This is a favorable rate."
-              d = {rate:message}		
+                message = "This is a favorable rate."
+                d = {rate:message}		
 				
-     print json.dumps(d, indent = 1, separators=(': '))			
-     sys.stdout.flush()
-     lastInstance = present
+        print json.dumps(d, indent = 1, separators=(': '))			
+        sys.stdout.flush()
+        lastInstance = present
